4526ae6502ec0fa227fc0c66afd9850dd72614a2
ColonolNutty <ColonolNutty@hotmail.com>
2017-02-19 21:37:47 -0700

    Poop spawns correctly now

--- items/generic/spawnpoop/spawnpoop.lua
+++ items/generic/spawnpoop/spawnpoop.lua
@@ -1,6 +1,6 @@
 function init()
 	script.setUpdateDelta(0)
-	world.spawnItem("poop", mcontroller.position(), effect.configParameter("spawnCount"))
+	world.spawnItem("poop", mcontroller.position(), effect.spawnCount)
 end
 
 function update(dt)
