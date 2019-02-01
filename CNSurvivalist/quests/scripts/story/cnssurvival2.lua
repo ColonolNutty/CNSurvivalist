7b7cc961c8cfa957c6df2c61e9a3f05937a6b67e
ColonolNutty <ColonolNutty@hotmail.com>
2019-02-01 16:38:41 -0700

    Increase debris tile damage

--- CNSurvivalist/quests/scripts/story/cnssurvival2.lua
+++
@@ -1,9 +0,0 @@
-require "/quests/scripts/main.lua"
-
-local originalQuestComplete = questComplete;
-
-function questComplete()
-  player.giveEssentialItem("beamaxe", "cnsdebris")
-  player.consumeItem({ name = "cnsdebris", count = 1 })
-  originalQuestComplete();
-end
